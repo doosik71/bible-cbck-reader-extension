52630f2422b609c5bf7b267917d3d4c9607b5c5f
Doosik Kim <doosik@etri.re.kr>
2025-01-02 10:30:05 +0900

    First commit.

---
+++ scripts/content.js
@@ -0,0 +1,36 @@
+const element_to_hide = ['header', 'footer', 'div.col-12.bottommargin-sm', 'div.sticky.sticky-right', 'div.gotoTop'];
+const element_to_font = ['*'];
+
+function bible_cbck_reader_init() {
+    element_to_hide.forEach(selector => {
+        const els = document.querySelectorAll(selector);
+        els.forEach(el => {
+            el.style.display = "none";
+        });
+    });
+
+    element_to_font.forEach(selector => {
+        const els = document.querySelectorAll(selector);
+        els.forEach(el => {
+            el.style.fontSize = "1em";
+        });
+    });
+
+    const elements = document.querySelectorAll('*');
+    elements.forEach(element => {
+        element.removeAttribute('class');
+        element.style.margin = '0';
+        element.style.padding = '0';
+    });
+
+    document.body.style.margin = "1em";
+}
+
+if (document.readyState === "loading") {
+    document.addEventListener("DOMContentLoaded", bible_cbck_reader_init);
+} else {
+    bible_cbck_reader_init();
+}
+
+
+// #main > div > div > div.off-10.col-2.off-10-large.col-2-large.off-9-small.col-3-small.align-right.sticky
